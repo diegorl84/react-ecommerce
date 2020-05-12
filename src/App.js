74e4ed6b468718b67e0ced28fc8a727bd8fdb12a
Diego <diegorl84@gmail.com>
2020-05-11 22:00:21 -0500

    Add withRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { Route, Switch, Link } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-     <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+        <Link to="/hats"></Link>
+      </Switch>
     </div>
   );
 }
